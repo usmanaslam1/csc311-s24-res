327a608ce941826e556c5c9d511d5824c8673445
usman <usman@localhost>
2024-03-10 19:30:03 -0700

    cleaned up and committed

--- CSC311-S24-RES/common/com/usman/csudh/csc311/datastructures/Queue.java
+++ CSC311-S24-RES/common/com/usman/csudh/csc311/datastructures/Queue.java
@@ -1,12 +1,45 @@
 package com.usman.csudh.csc311.datastructures;
 
+/**
+ * This interface represents a queue data structure. It contains all the key methods that a queue should support.
+ */
 public interface Queue {
 	
+	/**
+	 * Add an element to tail of the queue	
+	 * @param data the element to add to the queue
+	 */
 	public void add(int data);
+	
+	/**
+	 * Remove an element from the head of the queue
+	 * @return the element removed from the head of the queue
+	 * @throws QueueEmptyException if the queue is empty
+	 */
 	public int remove() throws QueueEmptyException;
+	
+	/**
+	 * Provides a view of the element at the head of the queue without removing it
+	 * @return the element at the head of the queue
+	 * @throws QueueEmptyException if the queue is empty
+	 */
 	public int peek() throws QueueEmptyException;
+	
+	/**
+	 * Returns the number of elements in the queue
+	 * @return the number of elements in the queue
+	 */
 	public int size();
+	
+	/**
+	 * Returns true if the queue is empty
+	 * @return true if the queue is empty
+	 */
 	public boolean isEmpty();
+	
+	/**
+	 * Clears the queue
+	 */
 	public void clear();
 
 }
--- CSC311-S24-RES/common/com/usman/csudh/csc311/datastructures/Stack.java
+++ CSC311-S24-RES/common/com/usman/csudh/csc311/datastructures/Stack.java
@@ -1,12 +1,49 @@
 package com.usman.csudh.csc311.datastructures;
 
+/**
+ * This is an interface for Stack data structure. It contains all the key methods that a stack should support.
+ */
 public interface Stack {
 	
+	/**
+	 * Adds an element to the top of the stack
+	 * @param data
+	 */
 	public void push(int data);
+	
+	/**
+	 * Removes an element from the top of the stack
+	 *
+	 * @return the element removed from the top of the stack
+	 * @throws StackEmptyException if the stack is empty
+	 */
 	public int pop() throws StackEmptyException;
+	
+	/**
+	 * Provides a view of the element at the top of the stack without removing it
+	 * 
+	 * @return the element at the top of the stack
+	 * @throws StackEmptyException if the stack is empty
+	 */
 	public int peek() throws StackEmptyException;
+	
+	/**
+	 * Returns the number of elements in the stack
+	 * 
+	 * @return the number of elements in the stack
+	 */
 	public int size();
+	
+	/**
+	 * Returns true if the stack is empty
+	 * 
+	 * @return true if the stack is empty
+	 */
 	public boolean isEmpty();
+	
+	/**
+	 * Clears the stack
+	 */
 	public void clear();
 
 }
